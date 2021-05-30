5f7eb8155813e9a3abd0b6b0b51f35dcb7a29c99
Jay Modi <mjrulesamrat@gmail.com>
2021-05-30 17:54:38 +0530

    Add promises states

--- promises_ex.js
+++ promises_ex.js
@@ -1,9 +1,11 @@
+// Promises are pending until they are fulfilled or rejected
 const doWork = new Promise((resolve, reject) => {
 
     setTimeout(() => {
         console.log("Hello world!")
         resolve([1, 2, 3])
         // reject("Error message here!!")
+        resolve([2, 5, 6]) // this will have no effect at all
     }, 2000);
 })
 
