b96a9d8958b37713a63ff5ebfb4c067f6884ef16
Jay Modi <mjrulesamrat@gmail.com>
2021-05-31 19:26:26 +0530

    Add await with promise function example

--- playground/async-await.js
+++ playground/async-await.js
@@ -1,6 +1,16 @@
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a+b)
+        }, 2000);
+    })
+}
+
 const doWork = async () => {
-    throw new Error("Something went wrong!!")
-    return "jay modi"
+    // throw new Error("Something went wrong!!")
+    // return "jay modi"
+    const sum = await add(1, 2)
+    return sum
 }
 
 doWork().then((result) => {
