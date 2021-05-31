50b1f50cd393866e902958de916b927a66b6e7dc
Jay Modi <mjrulesamrat@gmail.com>
2021-05-31 17:55:54 +0530

    Add tasks EP and model

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const app = require('./app')
 require('dotenv').config()
 
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const port = process.env.PORT
 
@@ -11,6 +12,17 @@ app.post('/users', (req, res) => {
 
     user.save().then(() => {
         res.status(200).send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    console.log(req.body)
+    const task = new Task(req.body)
+
+    task.save().then(() => {
+        res.status(200).send(task)
     }).catch((e) => {
         res.status(400).send(e)
     })
