62908883e4d2d1d4059e62c7720db6dcd2300ddc
Jay Modi <mjrulesamrat@gmail.com>
2021-05-31 18:17:12 +0530

    Add GET EP for users and tasks
    - Get User and Task by ID

--- src/index.js
+++ src/index.js
@@ -11,9 +11,30 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
 
     user.save().then(() => {
-        res.status(200).send(user)
+        res.status(201).send(user)
     }).catch((e) => {
         res.status(400).send(e)
+    })
+})
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if(!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
     })
 })
 
@@ -22,11 +43,32 @@ app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
     task.save().then(() => {
-        res.status(200).send(task)
+        res.status(201).send(task)
     }).catch((e) => {
         res.status(400).send(e)
     })
 })
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    Task.findById(req.params.id).then((task) => {
+        if(!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
