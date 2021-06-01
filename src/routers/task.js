a38f4bb207ef8bd1138843c6c68391e807088547
Jay Modi <mjrulesamrat@gmail.com>
2021-06-01 15:15:36 +0530

    Hash password & Patch method refactored
    Introduced Mongoose Schema to have pre, post hooks on data.

--- src/routers/task.js
+++ src/routers/task.js
@@ -41,8 +41,19 @@ router.get('/tasks/:id', async (req, res) => {
 
 
 router.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
     try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        const task = await Task.findById(req.params.id)
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
+        // const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true})
         res.send(task)
     } catch(e) {
         res.status(500).send()
--- src/routers/user.js
+++ src/routers/user.js
@@ -42,11 +42,23 @@ router.get('/users/:id', async (req, res) => {
 
 
 router.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
     const _id = req.params.id
+
     try {
-        const user = await User.findByIdAndUpdate(_id, req.body, {new: true})
+        const user = await User.findById(_id)
+
+        updates.forEach((update) => user[update] = req.body[update])
+
+        await user.save({new: true})
         res.send(user)
     } catch(e) {
+        console.log(e)
         res.status(500).send()
     }
 })
